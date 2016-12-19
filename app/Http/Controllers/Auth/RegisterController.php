428ceba20aa156017c22d80a204a01d65b33f0f3
Goldoni <fotsa.goldoni@yahoo.fr>
2016-12-19 18:17:03 +0100

    users table ajust and adding visitor

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -91,11 +91,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
             'confirmation_token' => str_replace('/', '', bcrypt(str_random(16))),
+            'visitor' => \Illuminate\Support\Facades\Request::ip()
         ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,33 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('role')->default('user');
             $table->string('name');
+            $table->string('first_name')->default('');
+            $table->string('last_name')->default('');
             $table->string('email')->unique();
             $table->string('password');
+            $table->ipAddress('visitor')->nullable();
+            $table->string('title',100)->default('');
+            $table->text('address')->nullable();
+            $table->string('zipcode',10)->nullable();
+            $table->string('city',60)->default('');
+            $table->unsignedSmallInteger('country')->default(0);
+            $table->string('phone1')->nullable();
+            $table->string('phone2')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('skype')->default('');
+            $table->unsignedTinyInteger('active_user')->default(0);
+            $table->string('personal_timezone',64)->default('');
+            $table->timestamp('last_login')->nullable();
+            $table->unsignedSmallInteger('login_failures')->default(0);
+            $table->unsignedSmallInteger('locked_until')->default(0);
+            $table->timestamp('last_password_change')->nullable();
+            $table->unsignedSmallInteger('language')->nullable();
+            $table->unsignedTinyInteger('system_account')->default(0);
+            $table->unsignedTinyInteger('is_readonly')->default(0);
+            $table->unsignedTinyInteger('deleted')->default(0);
+            $table->string('confirmation_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
